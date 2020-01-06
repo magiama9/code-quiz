9e1c94961bde16dbe15aac41354c70a8cc2dd4a3
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-06 13:35:57 -0500

    Initialize questions.js with list of questions.

---
+++ assets/scripts/questions.js
@@ -0,0 +1,72 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "What does 'NaN' mean in JavaScript?",
+    choices: [
+      "Not a Number",
+      "Not a Nincompoop",
+      "Never a Number",
+      "Not a NullValue"
+    ],
+    answer: "Not a Number"
+  },
+  {
+    title: "In Javascript, what operator breaks a string onto another line?",
+    choices: ["Slash /", "Backslash '\u005c'", "Pipe |", "Modulo %"],
+    answer: "Backslash '\u005c'"
+  },
+  {
+    title: "What is the AND operator in JavaScript?",
+    choices: ["&&", "++", "||", "i"],
+    answer: "&&"
+  },
+  {
+    title: "What would the expression ('Test' === Test) evaluate to?",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    title: "What would the expression ('Test' == Test) evaluate to?",
+    choices: ["True", "False"],
+    answer: "True"
+  },
+  {
+    title: "What does 'JSON' stand for?",
+    choices: [
+      "JavaScript Option Notation",
+      "JavaScript or Nothing",
+      "JavaScript Object Names",
+      "JavaScript Object Notation"
+    ],
+    answer: "JavaScript Object Notation"
+  },
+  {
+    title: "What method would you use to add an item to an array?",
+    choices: ["push()", "splice()", "append()", "addTo()"],
+    answer: "push()"
+  },
+  {
+    title: "What operator separates items in an array?",
+    choices: ["Comma ','", "Semicolon ';'", "Colon ':'", "Period '.'"],
+    answer: "alerts"
+  }
+];
+
+// function Get(yourUrl){
+//   var Httpreq = new XMLHttpRequest(); // a new request
+//   Httpreq.open("GET",yourUrl,false);
+//   Httpreq.send(null);
+//   return Httpreq.responseText;
+// }
+
+// var jsonObj = JSON.parse(Get("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple"));
+// console.log(jsonObj);
