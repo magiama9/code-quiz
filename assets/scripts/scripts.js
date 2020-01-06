9d959dbb2f1d6ca43c758e844e271eec7008248b
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-06 10:37:40 -0500

    Initialize /assets/scripts/scripts.js with application goals.

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,20 @@
+/*
+*****************************************
+# Code Quiz Homework
+
+* Timer based quiz application on coding fundamentals.
+
+* Start with landing page to begin quiz
+
+  *?? Offer the choice of different quizzes??
+
+  * Let user take the timed quiz
+    * Check user's answers
+    * Score user based on correct answers and time used
+    * Display final score
+    * Allow user to save their score
+    * Display a list of high scores
+
+*****************************************
+*/
+
