5cac4d85191166d9389418de979262011f63c15d
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-06 15:37:29 -0500

    Add functions to display question and check answers.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -16,5 +16,43 @@
     * Display a list of high scores
 
 *****************************************
+
+
+## Pseudocode
+
+  * Make the start quiz button load the first question
+  * Each question should automatically go to the next question if the answer is correct
+  * Display the correct answer if the answer was wrong.
+  
+  * At the end of the quiz, display the user's final score.
+  * Save the score to local storage
+
+*****************************************
+
 */
 
+// Display a question and its options as a list of buttons.
+function displayQuestion(i) {
+  $("#questionHead").text(questions[i].title);
+
+  for (var j = 0; j < questions[i].choices.length; j++) {
+    $("#questionOptions").append(
+      "<li><button class='choice' data-index=" +
+        j +
+        ">" +
+        questions[i].choices[j] +
+        "</button></li>"
+    );
+  }
+}
+// Check whether the selected answer is correct
+$("#questionBody").click(function(event, i) {
+  console.log(event.target);
+  var clickedIndex = parseInt($(event.target).attr("data-index"));
+  console.log(clickedIndex);
+  if (questions[i].choices[clickedIndex] === questions[i].answer) {
+    console.log("Correct");
+  } else console.log("Wrong");
+});
+
+displayQuestion(5);
